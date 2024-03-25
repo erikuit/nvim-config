f05d0f4a0f9cf60f195ee6deaf67aa983e73be94
Erik Uittenbogaard <erik@becurious.com>
2024-03-25 07:25:18 +0100

    Add keybinding for toggling Neotree

--- init.lua
+++ init.lua
@@ -162,6 +162,9 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- Toggle Neotree binding
+vim.keymap.set('n', '`', '<Cmd>Neotree toggle<CR>')
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
