0dfe5276b2464970b4df2f2a8b86c402c2aec855
Erik Uittenbogaard <erik@becurious.com>
2024-04-02 20:02:14 +0200

    Restore tab to copilot

--- init.lua
+++ init.lua
@@ -91,11 +91,11 @@ vim.keymap.set('n', 'S', ':%s//g<Left><Left>')
 vim.keymap.set('n', '`', '<Cmd>Neotree toggle<CR>')
 
 -- Override the default keybinds for Copilot
-vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
-  expr = true,
-  replace_keycodes = false,
-})
-vim.g.copilot_no_tab_map = true
+-- vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+--   expr = true,
+--   replace_keycodes = false,
+-- })
+-- vim.g.copilot_no_tab_map = true
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
