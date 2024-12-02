a28a84e417571c93158998fc7711b388e2c88939
erikuit <erik@becurious.com>
2024-12-02 20:22:19 +0100

    Add env variable to disable copilot on open

--- init.lua
+++ init.lua
@@ -191,7 +191,14 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
-  'github/copilot.vim',
+  {
+    'github/copilot.vim',
+    config = function()
+      if vim.env.COPILOT_DISABLED == '1' then
+        vim.cmd ':Copilot disable'
+      end
+    end,
+  },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
