1032a909ac9c638a45fda315cc0dc90b6ec63f11
erikuit <erik@becurious.com>
2025-09-07 21:01:10 +0200

    Try out kanagawa theme

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,9 +1,20 @@
 return {
+  -- {
+  --   'morhetz/gruvbox',
+  --   priority = 1000,
+  --   init = function()
+  --     vim.cmd.colorscheme 'gruvbox'
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --     -- Remove background color
+  --     vim.cmd 'autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE'
+  --   end,
+  -- },
   {
-    'morhetz/gruvbox',
+    'rebelot/kanagawa.nvim',
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'gruvbox'
+      vim.cmd.colorscheme 'kanagawa'
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
       -- Remove background color
